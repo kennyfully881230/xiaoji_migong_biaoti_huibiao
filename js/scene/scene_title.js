ecb8f195148acc508d8d559973b59dac8d5b52ce
Kenny Fully <kennyfully19881230@gmail.com>
2025-07-20 10:43:27 +0800

    updated title screen

--- js/scene/scene_title.js
+++ js/scene/scene_title.js
@@ -5,9 +5,14 @@ class SceneTitle extends Scene {
   }
 
   render() {
+    ctx.fillStyle = 'rgb(200,200,200)'
+    ctx.fillRect(0, 0, 320, 320)
     ctx.font = '40px FusionPixel'
     ctx.textAlign = 'center'
+    ctx.fillStyle = 'rgb(10,10,10)'
     ctx.fillText('小鸡迷宫', 160, 160)
+    ctx.font = '20px FusionPixel'
+    ctx.fillText('标题徽标', 160, 200)
   }
 
   update(options) {
